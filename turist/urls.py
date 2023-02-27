f9d0f2182b58901170673ae9a83c259b9f2b77cf
Dias-Ondiris <dias.ondiris2@gmail.com>
2023-02-27 18:09:06 +0600

    new

--- turist/urls.py
+++ turist/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'travel_agency'
 
 urlpatterns = [
-    path('users/<int:user_id>/', views.user_detail, name='user_detail'),
-    path('tours/<int:tour_id>/', views.tour_detail, name='tour_detail'),
-    path('tours/<int:tour_id>/add_comment/', views.add_comment, name='add_comment'),
+     
+     
 ]
